4f0a0462b63e5dd05f01c0042a518b76de38329b
Admin <Admin@LAPTOP-D38FQ3IJ>
2021-08-26 11:08:26 +0530

    new commit

--- ELf_16_19/src/actionsClassMethods/PoojaShree.java
+++ ELf_16_19/src/actionsClassMethods/PoojaShree.java
@@ -10,7 +10,7 @@ public class PoojaShree {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		
-		driver.get("https://www.facebook.com");	
+		driver.get("https://www.kite.com");	
 	}
 
 }
