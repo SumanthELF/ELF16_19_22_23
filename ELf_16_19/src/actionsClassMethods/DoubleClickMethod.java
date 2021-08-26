83856389f386becbfbb68fea9bd2aa5af18e236d
Admin <Admin@DESKTOP-940L4LM>
2021-08-26 11:06:35 +0530

    added new file

--- ELf_16_19/src/actionsClassMethods/DoubleClickMethod.java
+++ ELf_16_19/src/actionsClassMethods/DoubleClickMethod.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class DoubleClickMethod {
-
+   
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
--- ELf_16_19/src/actionsClassMethods/PoojaShree.java
+++ ELf_16_19/src/actionsClassMethods/PoojaShree.java
@@ -1,5 +1,13 @@
 package actionsClassMethods;
 
-public class PoojaShree {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class PoojaShree {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+
+	driver.get("https://www.skillrary.com/user/login");
+}
 }
