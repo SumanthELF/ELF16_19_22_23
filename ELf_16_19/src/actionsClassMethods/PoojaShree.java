967868b7a750707bbe439b8750a30561b66da918
Admin <Admin@LAPTOP-D38FQ3IJ>
2021-08-26 10:44:20 +0530

    new commit

--- ELf_16_19/src/actionsClassMethods/PoojaShree.java
+++ ELf_16_19/src/actionsClassMethods/PoojaShree.java
@@ -1,5 +1,16 @@
 package actionsClassMethods;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class PoojaShree {
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.instagram.com");	
+	}
 
 }
